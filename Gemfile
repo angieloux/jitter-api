fa2a2af218c0c9aa6d58b4681367666743d61afe
ANGIELOUX <angelajohnson943@gmail.com>
2022-06-18 00:22:11 +1000

    Add gems, seeds data, spec files

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem "database_cleaner", "~> 2.0"
+  gem "rspec-rails", "~> 5.1"
+  gem "factory_bot_rails", "~> 6.2"
 end
 
 group :development do
@@ -40,3 +43,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "puma", "~> 5.6"
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+if Message.count === 0 
+    Message.create(text: "Tempora ipsam perferendis iure accusamus mollitia labore nobis!
+    Accusantium in temporibus quod necessitatibus molestiae incidunt. Ratione eum nostrum natus! Sapiente dolor perferendis odit suscipit distinctio quae quos provident, ex aspernatur praesentium atque enim fugit corrupti consectetur in! Vel?")
+
+    Message.create(text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, commodi? Incidunt qui error cupiditate facilis fugiat, veniam officiis inventore quasi beatae corrupti laborum eius provident impedit iure sequi aut? Cumque?")
+
+    Message.create(text: "Praesentium, consequatur blanditiis? Explicabo, natus incidunt saepe omnis nulla excepturi accusantium ea minus dolore delectus officia earum voluptates sequi dolorum ipsum magnam aliquam harum iure error numquam. Nisi, suscipit nostrum.")
+end
